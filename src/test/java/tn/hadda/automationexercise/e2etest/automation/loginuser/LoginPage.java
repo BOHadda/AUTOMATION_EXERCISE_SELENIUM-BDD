686c9551e06d18cec25a31b9ff8799ffd22e421d
Zeineb SAMMOUD <zaineb.sammoud@gmail.com>
2024-03-06 22:33:57 +0100

    Update LoginPage.java

--- src/test/java/tn/hadda/automationexercise/e2etest/automation/loginuser/LoginPage.java
+++ src/test/java/tn/hadda/automationexercise/e2etest/automation/loginuser/LoginPage.java
@@ -11,9 +11,9 @@ import tn.hadda.automationexercise.e2etest.automation.utils.WaitMethods;
 public class LoginPage {
 	/* Locators */
 	final static String LOGIN_TO_YOUR_ACCOUNT = "//h2[contains(text(),'Login to your account')]";
-	final static String EMAIL_ADDRESS = "//input[contains(@data-qa,'login-email')]";
-	final static String PASSWORD_LOGIN = "//input[contains(@data-qa,'login-password')]";
-	final static String LOGIN_BUTTON = "//button[contains(@data-qa,'login-button')]";
+	final static String EMAIL_ADDRESS = "[data-qa='login-email']";
+	final static String PASSWORD_LOGIN = "[data-qa='login-password']";
+	final static String LOGIN_BUTTON = "[data-qa='login-button']";
 	final static String DELETE_ACCOUNT = "//a[@href='/delete_account']";
 	final static String MSG_DELETE_ACCOUNT = "//b[contains(text(),'Account Deleted!')]";
 	final static String MSG_INVALID_LOGIN = "//p[contains(text(),'Your email or password is incorrect!')]";
@@ -22,11 +22,11 @@ public class LoginPage {
 
 	@FindBy(how = How.XPATH, using = LOGIN_TO_YOUR_ACCOUNT)
 	public static WebElement loginToYourAccount;
-	@FindBy(how = How.XPATH, using = EMAIL_ADDRESS)
+	@FindBy(how = How.CSS, using = EMAIL_ADDRESS)
 	public static WebElement emailAddress;
-	@FindBy(how = How.XPATH, using = PASSWORD_LOGIN)
+	@FindBy(how = How.CSS, using = PASSWORD_LOGIN)
 	public static WebElement passwordLogin;
-	@FindBy(how = How.XPATH, using = LOGIN_BUTTON)
+	@FindBy(how = How.CSS, using = LOGIN_BUTTON)
 	public static WebElement loginBtn;
 	@FindBy(how = How.XPATH, using = DELETE_ACCOUNT)
 	public static WebElement deleteAccount;
